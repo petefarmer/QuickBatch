527f6aa9ae4fd26a0efb907c74dd62e912a8907d
Pete Farmer <pete.farmer@gmail.com>
2025-03-25 20:15:17 +0100

    Fix ECCN Keys controller: - Add proper module namespace - Add authentication and admin checks - Add require_admin method - Ensure proper inheritance and security

--- app/controllers/admin/eccn_keys_controller.rb
+++ app/controllers/admin/eccn_keys_controller.rb
@@ -1,50 +1,60 @@
-class Admin::EccnKeysController < ApplicationController
-  before_action :set_eccn_key, only: [:show, :edit, :update, :destroy]
+module Admin
+  class EccnKeysController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_admin
+    before_action :set_eccn_key, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @eccn_keys = EccnKey.all
-  end
-
-  def show
-  end
-
-  def new
-    @eccn_key = EccnKey.new
-  end
-
-  def edit
-  end
-
-  def create
-    @eccn_key = EccnKey.new(eccn_key_params)
-
-    if @eccn_key.save
-      redirect_to admin_eccn_keys_path, notice: 'ECCN Key was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
+    def index
+      @eccn_keys = EccnKey.all
     end
-  end
 
-  def update
-    if @eccn_key.update(eccn_key_params)
-      redirect_to admin_eccn_keys_path, notice: 'ECCN Key was successfully updated.'
-    else
-      render :edit, status: :unprocessable_entity
+    def show
     end
-  end
 
-  def destroy
-    @eccn_key.destroy
-    redirect_to admin_eccn_keys_path, notice: 'ECCN Key was successfully deleted.'
-  end
+    def new
+      @eccn_key = EccnKey.new
+    end
 
-  private
+    def edit
+    end
 
-  def set_eccn_key
-    @eccn_key = EccnKey.find(params[:id])
-  end
+    def create
+      @eccn_key = EccnKey.new(eccn_key_params)
 
-  def eccn_key_params
-    params.require(:eccn_key).permit(:name, :description)
+      if @eccn_key.save
+        redirect_to admin_eccn_keys_path, notice: 'ECCN Key was successfully created.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @eccn_key.update(eccn_key_params)
+        redirect_to admin_eccn_keys_path, notice: 'ECCN Key was successfully updated.'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @eccn_key.destroy
+      redirect_to admin_eccn_keys_path, notice: 'ECCN Key was successfully deleted.'
+    end
+
+    private
+
+    def set_eccn_key
+      @eccn_key = EccnKey.find(params[:id])
+    end
+
+    def eccn_key_params
+      params.require(:eccn_key).permit(:name, :description)
+    end
+
+    def require_admin
+      unless current_user&.admin?
+        redirect_to root_path, alert: 'You are not authorized to access this area.'
+      end
+    end
   end
 end 
