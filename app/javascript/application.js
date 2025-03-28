4c5050a4acb8a2d7bedc8616f7c1b4e6d915c9dd
Pete Farmer <pete.farmer@gmail.com>
2025-03-28 10:59:54 +0100

    Add root path link to QuickBatch logo and fix navbar styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import Alpine from "alpinejs"
+
+// Initialize Alpine.js
+document.addEventListener("turbo:load", () => {
+  window.Alpine = Alpine
+  Alpine.start()
+})
